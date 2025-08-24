4fd8499696ef2b4617632d63057315e8a37f0298
DischordDynne <wgibbs08@outlook.com>
2025-08-24 17:22:36 -0400

    create proper analysis example

--- Veritas/example.jl
+++ Veritas/example.jl
@@ -1,7 +1,12 @@
 
 
-@pre x > 0
-@post _result != 0
-function main()
-
+# Asserts that for every input x and y,
+@pre y != 0
+@post result * y = x
+function divide(x, y)
+    return x / y
 end
+
+# The trouble with functions like this is that X, Y, and result are all unknown
+# types. Veritas will test treating X and Y as floating points, while stating that
+# full testing is unavailable due to dynamic typing.
